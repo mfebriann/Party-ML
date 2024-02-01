aa6171573820e5b6500dfcd4e4de7fe56bb547fa
mfebriann <muhammadfebriyan86@gmail.com>
2024-02-01 22:34:55 +0700

    update content with date and description

--- src/pages/ContentEvent.jsx
+++ src/pages/ContentEvent.jsx
@@ -1,6 +1,6 @@
 import { Link, useParams } from "react-router-dom";
-import { events, linkGroup } from "../utils";
-import { useEffect, useState } from "react";
+import { events } from "../utils";
+import { Fragment, useEffect, useState } from "react";
 
 const ContentEvent = () => {
   const { id } = useParams();
@@ -11,18 +11,53 @@ const ContentEvent = () => {
     setContentEvent(content);
   }, [id]);
 
+  const buttonActive =
+    "bg-gradient-to-r from-sky-400 to-blue-500 text-white hover:opacity-90 ease-in duration-200 transition";
+  const buttonDisable = "pointer-events-none bg-slate-300 text-white/80";
+
   return (
     <main className="container mx-auto h-screen bg-white p-5">
-      <article>
+      <article className="pb-5">
         <h1 className="text-2xl font-bold text-slate-900 sm:text-3xl">
           {contentEvent.title}
         </h1>
-        <p className="mt-3 text-slate-700">{contentEvent.description}</p>
+        <p className="mt-2 text-sm text-slate-400">
+          {contentEvent.startdate} - {contentEvent.enddate}
+        </p>
+        <p className="mt-4 text-slate-700">
+          {Object.keys(contentEvent).length > 0 &&
+            contentEvent.description.split("\n").map((paragraph, index) => (
+              <Fragment key={index}>
+                {paragraph.includes("[") && paragraph.includes("]") ? (
+                  paragraph.split(/\[(.*?)]/).map((text, i) => (
+                    <Fragment key={i}>
+                      {i % 2 === 0 ? (
+                        text
+                      ) : (
+                        <a
+                          href={text}
+                          target="_blank"
+                          className="text-blue-500 underline"
+                          rel="noreferrer"
+                        >
+                          {text.split("]")[0]}
+                        </a>
+                      )}
+                    </Fragment>
+                  ))
+                ) : (
+                  // Jika tidak ada tautan, render sebagai teks biasa
+                  <span>{paragraph}</span>
+                )}
+                <br />
+              </Fragment>
+            ))}
+        </p>
         <a
-          href={linkGroup}
-          className="mb-12 mt-8 block rounded-xl bg-gradient-to-r from-sky-400 to-blue-500 p-3 text-center font-medium  text-white"
+          href={contentEvent.isExpired ? "#" : contentEvent.link}
+          className={`mb-12 mt-8 block rounded-xl p-3 text-center font-medium ${contentEvent.isExpired ? buttonDisable : buttonActive}`}
         >
-          Gabung Sekarang
+          Gabung Grup
         </a>
         <Link to={"/"} className="mx-auto block w-max text-blue-600 underline">
           Kembali
--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,18 @@ export const events = [
   {
     id: 1,
     title: "Giveaway 185 diamond",
-    description:
-      "Setiap 2 bulan sekali kami mengadakan event yaitu giveaway 185 diamond kepada para member grup, dan kami hanya memberikan untuk 5 member grup secara acak. Event akan dimulai pada 10 Februari 2024 dan akan berakhir pada akhir bulan Februari sekaligus menentukan siapa saja pemenang giveaway ini.",
+    description: `Setiap 2 bulan sekali kami mengadakan event yaitu giveaway 185 diamond kepada para member grup, dan kami hanya memberikan untuk 5 member grup secara acak. Event akan dimulai pada 10 Februari 2024 dan akan berakhir pada 17 Februari 2024 sekaligus menentukan siapa saja pemenang giveaway ini. 
+    
+    Gimana cara mengikutinya?
+    1. Bergabung dengan grup kita di [https://t.me/party_ml]
+    2. Follow channel telegram [https://t.me/crewjoki]
+    3. Follow instagram [https://instagram.com/crewjoki]
+    4. Jangan lupa screenshot nomor 2 dan 3, untuk bukti
+    
+    Jika semua sudah terpenuhi maka kamu bisa chat admin di telegram yaitu [https://t.me/riann18] dan memberikan bukti screenshot nya + username telegram kamu. Dan kami akan menyimpan username telegram kamu untuk diundi nanti pada tanggal 17 Februari 2024`,
+    startdate: "01 Februari 2024",
+    enddate: "17 Februari 2024",
+    isExpired: false,
+    link: linkGroup,
   },
 ];
